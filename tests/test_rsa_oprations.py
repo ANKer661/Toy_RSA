bf270d052cde161542ba4e3581c9327bc8c8cb32
bobo_win <1229316440@qq.com>
2024-06-25 22:20:48 +0800

    test: Update RSA pipeline tests

--- tests/test_rsa_oprations.py
+++ tests/test_rsa_oprations.py
@@ -9,16 +9,16 @@ class TestRSAOprations(unittest.TestCase):
         for _ in range(8):
             keys = generate_keypair(1024)
             self.assertEqual(
-                rsa_pipeline("Hello, World!", keys),
+                rsa_pipeline("Hello, World!", keys)[0],
                 "Hello, World!"
             )
             m = "The Python interpreter has a number of functions"
-            self.assertEqual(rsa_pipeline(m, keys), m)
+            self.assertEqual(rsa_pipeline(m, keys)[0], m)
             m = "Hi John."
-            self.assertEqual(rsa_pipeline(m, keys), m)
+            self.assertEqual(rsa_pipeline(m, keys)[0], m)
             m = "Good bye, John."
-            self.assertEqual(rsa_pipeline(m, keys), m)
+            self.assertEqual(rsa_pipeline(m, keys)[0], m)
             m = "kljasdiwnlkjxzlxkcj8iuwhlaksdjkqwioi1"
-            self.assertEqual(rsa_pipeline(m, keys), m)
+            self.assertEqual(rsa_pipeline(m, keys)[0], m)
             m = "香草丝绒拿铁 无奶油/少少甜"
-            self.assertEqual(rsa_pipeline(m, keys), m)
+            self.assertEqual(rsa_pipeline(m, keys)[0], m)
